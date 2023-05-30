cc56811e6cae49b618343269fa9acc2a4a5a80b5
Usama Husnain <usama.husnain@nexarise.com>
2023-05-30 16:46:46 +0500

    super user added

--- todo/settings.py
+++ todo/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-
+    
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
     #     'NAME': 'todo',
